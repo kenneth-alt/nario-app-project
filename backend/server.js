e207284fa055577b3e31302ef59099c454bfe1e1
kenneth-alt <kenneth.ugo15@gmail.com>
2023-03-18 09:20:23 -0600

    init server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log('Hello world')
+import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
