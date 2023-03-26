768f07ef964c22655e6ed4349eaabbc4cfa7e5eb
kenneth-alt <kenneth.ugo15@gmail.com>
2023-03-26 00:34:47 -0600

    update entryRoutes.js

--- backend/routes/entryRoutes.js
+++ backend/routes/entryRoutes.js
@@ -5,7 +5,44 @@ import {
     updateEntry, 
     deleteEntry } from "../controllers/entryController.js"
 
-export const router = express.Router()
+export const entriesRouter = express.Router()
 
-router.route('/').get(getEntries).post(createEntry)
-router.route('/:id').put(updateEntry).delete(deleteEntry)
+entriesRouter.post('/', async (req, res) => {
+    const newEntry = req.body
+    try {
+        const id = await createEntry(newEntry)
+        res.send(`Success, added ${newEntry.topic} - ${id}`)
+    } catch (error) {
+        res.status(403).send(error.message)
+    }
+})
+
+entriesRouter.get('/', async (req, res) => {
+    try {
+        const allEntries = await getEntries()
+        res.send(allEntries)
+    } catch (error) {
+        res.status(404).send(error.message)
+    }  
+})
+
+entriesRouter.patch('/:id', async (req, res) => {
+    const id = req.params.id
+    const newEntry = req.body
+    try {
+        const updatedEntry = await updateEntry(id, newEntry)
+        res.send(updatedEntry)
+    } catch (error) {
+        res.status(403).send(error.message)
+    }
+})
+
+entriesRouter.delete('/:id', async (req, res) => {
+    const id = req.params.id
+    try {
+        const deletedEntry = await deleteEntry(id)
+        res.send(deletedEntry)
+    } catch (error) {
+        res.status(403).send(error.message)
+    }
+})
