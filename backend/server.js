84422beee8b04aed4baab42eda926dd3cae25f7b
kenneth-alt <kenneth.ugo15@gmail.com>
2023-03-25 23:46:15 -0600

    minor changes to server.js

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,10 @@ import { router } from "./routes/entryRoutes.js"
 
 dotenv.config()
 
-connectDB()
-
 const app = express()
-const port = process.env.PORT 
+const port = process.env.PORT || 5000
+
+connectDB()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false}))
