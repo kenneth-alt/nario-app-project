bce9980c668ae8989faf312f98916c7838dbcf35
kenneth-alt <kenneth.ugo15@gmail.com>
2023-03-30 20:22:09 -0600

    minor changes to schemas

--- backend/models/entrySchema.js
+++ backend/models/entrySchema.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const entrySchema = new mongoose.Schema({
     _id: { type: String, required: true },
     userEmail: {type: String, required: true, ref: 'User'},
-    //user: {type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User'},
+    // user: {type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User'},
     topic: { type: String, required: true },
     content: { type: String, required: true },
     status: { type: String, default: 'started' },
